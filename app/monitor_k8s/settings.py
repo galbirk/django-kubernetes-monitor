3bba34d9838fd35ffb5415e9b2c130c84fb2959a
Gal Birkman <galbirkman@gmail.com>
2020-11-09 19:17:16 +0200

    Fixed settings

--- app/monitor_k8s/settings.py
+++ app/monitor_k8s/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'monitor/static/monitor/'
+STATIC_ROOT = 'monitor/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
