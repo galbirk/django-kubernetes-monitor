4162a3cd9a0fd68276600b4855abd69650bd3732
Gal Birkman <galbirkman@gmail.com>
2020-11-09 20:00:58 +0200

    Fixed settings

--- app/monitor_k8s/settings.py
+++ app/monitor_k8s/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'monitor/static/'
+#STATIC_ROOT = 'monitor/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
