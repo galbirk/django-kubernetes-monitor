bd6349dd9c13344fa280878ddb2f9b863f25b93c
Gal Birkman <galbirkman@gmail.com>
2020-11-09 19:07:18 +0200

    Fixed run_server.sh

--- app/run_server.sh
+++ app/run_server.sh
@@ -2,7 +2,7 @@
 # start django server
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; 
 then
-    python manage.py collectstatic
+    python manage.py collectstatic --noinput
 		python manage.py makemigrations
     python manage.py migrate
     python manage.py createsuperuser --no-input
