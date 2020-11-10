de3a80d2bf0d575cfe17fba74cf86e7e16e42445
Gal Birkman <galbirkman@gmail.com>
2020-11-10 20:25:21 +0200

    Added tests image and run_server script

---
+++ app/run_server_test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# start django server
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; 
+then
+    python manage.py collectstatic --noinput
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py createsuperuser --no-input
+fi
+gunicorn --bind :8000 --workers 3 monitor_k8s.wsgi
