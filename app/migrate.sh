7bb7226b9906e25260a970577948f6fc72b6baf6
Gal Birkman <galbirkman@gmail.com>
2020-11-14 21:43:43 +0200

    Fixed migrate.sh

--- app/migrate.sh
+++ app/migrate.sh
@@ -3,5 +3,5 @@ if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ;
 then
     python manage.py makemigrations
     python manage.py migrate
-    python manage.py createsuperuser --no-input
+    python manage.py createsuperuser --noinput
 fi
